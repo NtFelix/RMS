94717db63176914ae77807cbb96a46a92adaba49
Felix <felix.plant@hotmail.com>
2025-02-07 23:38:00 +0100

    funktioniert (nicht sicher)

---
+++ assets/scripts/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.38.4/+esm'
+
+const supabaseUrl = 'https://dmrglslyrrqjlomjsbas.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtcmdsc2x5cnJxamxvbWpzYmFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA4MTA0MzUsImV4cCI6MjAzNjM4NjQzNX0.pzm4EYAzxkCU-ZKAgybeNK9ERgdqBVdHlZbp1aEMndk';
+export const supabase = createClient(supabaseUrl, supabaseKey);
