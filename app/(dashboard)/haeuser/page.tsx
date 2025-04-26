3f748a02d18548b6597dc58ba3739fd0321ebeec
Felix <felix.plant@web.de>
2025-04-26 12:17:37 +0200

    Dynamische seiten

--- app/(dashboard)/haeuser/page.tsx
+++ app/(dashboard)/haeuser/page.tsx
@@ -1,6 +1,5 @@
+export const dynamic = 'force-dynamic';
 "use client"
-
-export const dynamic = 'force-dynamic'
 
 import { useState, useRef, useCallback, useEffect } from "react" // Add useCallback and useEffect
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
