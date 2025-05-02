49d319487c6cb795775eccfb12bfdbcd15a764dc
Felix <felix.plant@web.de>
2025-05-02 11:33:07 +0200

    Verbesserungen der Transaktionsansicht
    - Sortierung der Transaktionen nach Datum (neueste zuerst)
    - Dynamische Jahre-Selektion basierend auf Transaktionsdaten
    - Anpassung der Datumsanzeige auf DD.MM.YYYY Format

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("217e8d05922ccd2c")
+/******/ 		__webpack_require__.h = () => ("a8509434cf3c204c")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
