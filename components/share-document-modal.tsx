2d6694d544e0dcf7e8c96f6c40e21a6faeba5cd4
Felix <felix.plant@hotmail.com>
2025-09-06 22:18:46 +0200

    Improve share modal spacing
    - Reduce vertical spacing from space-y-6 to space-y-4
    - Reduce padding from py-4 to py-2
    - Brings share button closer to bottom of modal for better UX

--- components/share-document-modal.tsx
+++ components/share-document-modal.tsx
@@ -127,7 +127,7 @@ export function ShareDocumentModal({
           </DialogDescription>
         </DialogHeader>
         
-        <div className="space-y-6 py-4">
+        <div className="space-y-4 py-2">
           {/* Expiration Time Selection */}
           <div className="space-y-2">
             <Label htmlFor="expiration">
