e6010132d582cfc3dee43cbf524405553a124a7c
Felix <felix.plant@hotmail.com>
2025-09-18 22:08:41 +0200

    Revert "Enhance custom combobox input field spacing"
    This reverts commit 2122fb92d75512001fbd881627a97f6180e50df1.

--- components/ui/custom-combobox.tsx
+++ components/ui/custom-combobox.tsx
@@ -373,7 +373,7 @@ export function CustomCombobox({
         >
           <div className="flex flex-col">
             {/* Custom search input with aggressive focus management */}
-            <div className="flex items-center border-b px-4 py-3">
+            <div className="flex items-center border-b px-3 py-2">
               <input
                 ref={inputRef}
                 type="text"
