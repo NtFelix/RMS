88158ffa798a2be50cc8d788d941bb9d83a370a8
Felix <felix.plant@hotmail.com>
2026-05-17 13:26:43 +0200

    fix(chart): separate ResponsiveContainer props from div attributes to prevent DOM pollution

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -46,6 +46,18 @@ const ChartContainer = React.forwardRef<
   const uniqueId = React.useId()
   const chartId = `chart-${id || uniqueId.replace(/:/g, "")}`
 
+  const {
+    aspect,
+    width,
+    height,
+    minWidth,
+    minHeight,
+    maxHeight,
+    debounce,
+    onResize,
+    ...divProps
+  } = props
+
   return (
     <ChartContext.Provider value={{ config }}>
       <div
@@ -55,10 +67,19 @@ const ChartContainer = React.forwardRef<
           "flex aspect-video justify-center text-xs [&_.recharts-cartesian-axis-tick_text]:fill-muted-foreground [&_.recharts-cartesian-grid_line[stroke='#ccc']]:stroke-border/50 [&_.recharts-curve.recharts-tooltip-cursor]:stroke-border [&_.recharts-dot[stroke='#fff']]:stroke-transparent [&_.recharts-layer]:outline-hidden [&_.recharts-polar-grid_[stroke='#ccc']]:stroke-border [&_.recharts-radial-bar-background-sector]:fill-muted [&_.recharts-rectangle.recharts-tooltip-cursor]:fill-muted [&_.recharts-reference-line_[stroke='#ccc']]:stroke-border [&_.recharts-sector[stroke='#fff']]:stroke-transparent [&_.recharts-sector]:outline-hidden [&_.recharts-surface]:outline-hidden",
           className
         )}
-        {...props}
+        {...divProps}
       >
         <ChartStyle id={chartId} config={config} />
-        <RechartsPrimitive.ResponsiveContainer {...props}>
+        <RechartsPrimitive.ResponsiveContainer
+          aspect={aspect}
+          width={width}
+          height={height}
+          minWidth={minWidth}
+          minHeight={minHeight}
+          maxHeight={maxHeight}
+          debounce={debounce}
+          onResize={onResize}
+        >
           {children}
         </RechartsPrimitive.ResponsiveContainer>
       </div>
